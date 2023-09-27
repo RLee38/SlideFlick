52494742baee5c87ed078fa38e06d90a97e84042
Ryan Lee <RLEE2597@stu.gusd.net>
2023-09-27 11:44:47 -0700

    Working Camera intake and display with handtracking

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 # This is a sample Python script.
+import cv2
 import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
+#from mediapipe.tasks import python
+#from mediapipe.tasks.python import vision
 
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
 
+cap = cv2.VideoCapture(0)
+hands = mp_hands.Hands()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
+while True:
+    data, image = cap.read()
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    results = hands.process(image)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                image,
+                hand_landmarks, mp_hands.HAND_CONNECTIONS)
+    cv2.imshow('Handtracker', image)
+    cv2.waitKey(1)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

7bf6bf3d658a6a2e6c96a35ac1047b5a2af1466c
nw-stolidus <noahcwalch@gmail.com>
2023-10-18 12:22:11 -0700

    main for google slides manipulation without credentials file

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
 # This is a sample Python script.
-import cv2
-import mediapipe as mp
 import os
+import json
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 import msvcrt
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
 
-cap = cv2.VideoCapture(0)
-hands = mp_hands.Hands()
-CREDS_FILE = 'credentials.json'
-SCOPES = ['https://www.googleapis.com/auth/presentations.readonly']
+CREDS_FILE = 'C:/Users/NWAL0169/PycharmProjects/SlideFlick/credentials.json'
+CLIENT_ID = 'C:/Users/NWAL0169/PycharmProjects/SlideFlick/credentials.json'
+CLIENT_SECRET = 'C:/Users/NWAL0169/PycharmProjects/SlideFlick/client_secrets.json'
+SCOPES = ['https://www.googleapis.com/auth/presentations.readonly', 'https://www.googleapis.com/auth/drive',
+          'https://www.googleapis.com/auth/gmail.readonly']
 
-# slides-id = 1Z1Q4jy5pL9NprL2hWkiA3UR173h50yRCAatoyGM8Cfw
+# slides-id = ‘1Z1Q4jy5pL9NprL2hWkiA3UR173h50yRCAatoyGM8Cfw’
+
+
+REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
+
+
+def get_refresh_token():
+    try:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'C:/Users/NWAL0169/PycharmProjects/SlideFlick/client_secrets.json',
+            SCOPES,
+            redirect_uri=REDIRECT_URI
+        )
+
+        # Redirect user to OAuth 2.0 consent page
+        authorization_url, _ = flow.authorization_url()
+        print(_)
+        print('Please go to this URL to authorize access:', authorization_url)
+
+        # Obtain authorization code from user
+        authorization_response = input('Paste the full redirect URL here: ')
+        flow.fetch_token(authorization_response=authorization_response)
+
+        # Access and store the refresh token
+        refresh_token = flow.credentials.refresh_token()
+        print('Refresh Token:', refresh_token)
+
+    except Exception as e:
+        print('An error occurred during token retrieval:', str(e))
 
 
 def authenticate():
@@ -27,25 +52,25 @@ def authenticate():
     if not creds or not creds.valid:
         flow = InstalledAppFlow.from_client_secrets_file('client_secrets.json', SCOPES)
         creds = flow.run_local_server(port=0)
-
+        print(flow.credentials.refresh_token())
         with open(CREDS_FILE, 'w') as token:
             token.write(creds.to_json())
 
-    return creds
+        return creds
 
 
-def get_presentation_slides(presentation_idd):
+def get_presentation_slides(presentation_id):
     creds = authenticate()
     service = build('slides', 'v1', credentials=creds)
 
-    presentation = service.presentations().get(presentationId=presentation_idd).execute()
-    slidess = presentation.get('slides', [])
+    presentation = service.presentations().get(presentationId=presentation_id).execute()
+    slides = presentation.get('slides', [])
 
-    return slidess
+    return slides
 
 
-def navigate_through_slides(slidess):
-    if not slidess:
+def navigate_through_slides(slides):
+    if not slides:
         print('No slides found.')
         return
 
@@ -55,10 +80,8 @@ def navigate_through_slides(slidess):
         os.system('cls' if os.name == 'nt' else 'clear')  # Clear console
         print(f'Currently on Slide {current_slide_index + 1}')
         print('Press "n" for next slide, "p" for previous slide, or "q" to quit.')
-
         # Read keyboard input
         key = msvcrt.getch().decode('utf-8').lower()
-        # Currently linked to keyboard REPLACE WITH HAND GESTURES as variables
         if key == 'n':
             current_slide_index = (current_slide_index + 1) % len(slides)
         elif key == 'p':
@@ -73,21 +96,8 @@ def navigate_through_slides(slidess):
 
 if __name__ == '__main__':
     # Replace with your actual presentation ID
+    # os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
     presentation_id = '1Z1Q4jy5pL9NprL2hWkiA3UR173h50yRCAatoyGM8Cfw'
+    get_refresh_token()
     slides = get_presentation_slides(presentation_id)
-    # Change navigate method and put in handtracking loop with gesture recognition
     navigate_through_slides(slides)
-
-    while True:
-        data, image = cap.read()
-        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-        results = hands.process(image)
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    image,
-                    hand_landmarks, mp_hands.HAND_CONNECTIONS)
-        cv2.imshow('Handtracker', image)
-        cv2.waitKey(1)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
